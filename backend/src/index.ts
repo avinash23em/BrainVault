07a72257de7a48ce9a8d7236fbcb3233f29b9993
avinash23em <ap23vi@gmail.com>
2026-05-03 17:35:48 +0530

    initial

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Configure CORS
-const allowedOrigins = ["https://brain-vault-xi.vercel.app", "http://localhost:5000", "http://localhost:5173"];
+const allowedOrigins = ["https://brain-vault-xi.vercel.app", "https://brainvault-daam.onrender.com","http://localhost:5000", "http://localhost:5173"];
 
 app.use(
   cors({
